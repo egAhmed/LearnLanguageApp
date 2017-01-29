3262e20887645b683a491dad5d45d44fa4f8f766
Ejstn <aceton@centrum.cz>
2017-01-30 00:36:09 +0100

    20

--- app/src/main/java/cz/ejstn/learnlanguageapp/slovicka/Kategorie3JidloAPiti.java
+++ app/src/main/java/cz/ejstn/learnlanguageapp/slovicka/Kategorie3JidloAPiti.java
@@ -37,6 +37,34 @@ public class Kategorie3JidloAPiti {
         slovicka.add(new Slovicko("salad", "salát", R.drawable.jidlo_salat, R.raw.song));
         slovicka.add(new Slovicko("donut", "kobliha", R.drawable.jidlo_kobliha, R.raw.song));
 
+        slovicka.add(new Slovicko("coffe", "káva", R.drawable.jidlo_chleba, R.raw.song));
+        slovicka.add(new Slovicko("ice cream", "zmrzlina",R.drawable.jidlo_susenka, R.raw.song));
+        slovicka.add(new Slovicko("french fries", "hranolky", R.drawable.jidlo_maso, R.raw.song));
+        slovicka.add(new Slovicko("pizza", "pizza", R.drawable.jidlo_brambora, R.raw.song));
+        slovicka.add(new Slovicko("cheese", "sýr", R.drawable.jidlo_ryze, R.raw.song));
+        slovicka.add(new Slovicko("peanut", "burák", R.drawable.jidlo_testoviny, R.raw.song));
+        slovicka.add(new Slovicko("lolipop", "lízátko", R.drawable.jidlo_jablko, R.raw.song));
+        slovicka.add(new Slovicko("hotdog", "párek v rohlíku", R.drawable.jidlo_dort, R.raw.song));
+        slovicka.add(new Slovicko("tomato", "rajče", R.drawable.jidlo_salat, R.raw.song));
+        slovicka.add(new Slovicko("watermelon", "vodní meloun", R.drawable.jidlo_kobliha, R.raw.song));
+
+        slovicka.add(new Slovicko("bacon", "slanina", R.drawable.jidlo_chleba, R.raw.song));
+        slovicka.add(new Slovicko("cucumber", "okurka",R.drawable.jidlo_susenka, R.raw.song));
+        slovicka.add(new Slovicko("pear", "hruška", R.drawable.jidlo_maso, R.raw.song));
+        slovicka.add(new Slovicko("banana", "banán", R.drawable.jidlo_brambora, R.raw.song));
+        slovicka.add(new Slovicko("pancake", "palačinka", R.drawable.jidlo_ryze, R.raw.song));
+        slovicka.add(new Slovicko("omelette", "omeleta", R.drawable.jidlo_testoviny, R.raw.song));
+        slovicka.add(new Slovicko("carrot", "mrkev", R.drawable.jidlo_jablko, R.raw.song));
+        slovicka.add(new Slovicko("strawberry", "jahoda", R.drawable.jidlo_dort, R.raw.song));
+        slovicka.add(new Slovicko("milk", "mléko", R.drawable.jidlo_salat, R.raw.song));
+        slovicka.add(new Slovicko("tea", "čaj", R.drawable.jidlo_kobliha, R.raw.song));
+
+        slovicka.add(new Slovicko("wine", "víno", R.drawable.jidlo_jablko, R.raw.song));
+        slovicka.add(new Slovicko("chocolate", "čokoláda", R.drawable.jidlo_dort, R.raw.song));
+        slovicka.add(new Slovicko("beer", "pivo", R.drawable.jidlo_salat, R.raw.song));
+        slovicka.add(new Slovicko("alcohol", "alkohol", R.drawable.jidlo_kobliha, R.raw.song));
+        slovicka.add(new Slovicko("candy", "bonbón", R.drawable.jidlo_kobliha, R.raw.song));
+
 
         return slovicka;
     }
--- app/src/main/java/cz/ejstn/learnlanguageapp/slovicka/Kategorie7Sport.java
+++ app/src/main/java/cz/ejstn/learnlanguageapp/slovicka/Kategorie7Sport.java
@@ -37,6 +37,19 @@ public class Kategorie7Sport {
         slovicka.add(new Slovicko("skiing", "lyžování", R.drawable.sport_lyzovani, R.raw.song));
         slovicka.add(new Slovicko("swimming", "plavání", R.drawable.sport_plavani, R.raw.song));
 
+        slovicka.add(new Slovicko("wrestling", "zápasení", R.drawable.sport_zapaseni, R.raw.song));
+        slovicka.add(new Slovicko("boxing", "box", R.drawable.sport_box, R.raw.song));
+        slovicka.add(new Slovicko("ice skating", "bruslení", R.drawable.sport_brusleni, R.raw.song));
+        slovicka.add(new Slovicko("golf", "golf", R.drawable.sport_golf, R.raw.song));
+        slovicka.add(new Slovicko("fencing", "šerm", R.drawable.sport_serm, R.raw.song));
+        slovicka.add(new Slovicko("handball", "házená", R.drawable.sport_hazena, R.raw.song));
+        slovicka.add(new Slovicko("horse racing", "jízda na koni", R.drawable.sport_jizda_na_koni, R.raw.song));
+        slovicka.add(new Slovicko("snowboarding", "jízda na snowboardu", R.drawable.sport_snowboard, R.raw.song));
+        slovicka.add(new Slovicko("surfing", "serfování", R.drawable.sport_serfovani, R.raw.song));
+        slovicka.add(new Slovicko("rowing", "veslování", R.drawable.sport_veslovani, R.raw.song));
+        slovicka.add(new Slovicko("field hockey", "pozemní hokej", R.drawable.sport_pozemni_hokej, R.raw.song));
+        slovicka.add(new Slovicko("martial arts", "bojová umění", R.drawable.sport_bojova_umeni, R.raw.song));
+
 
         return slovicka;
     }
