cd42e452d74717423354636aa29ce327a76018a1
Ejstn <aceton@centrum.cz>
2017-02-06 21:22:57 +0100

    46 rozdělané menu - funguje share a infor

---
+++ app/src/main/java/cz/ejstn/learnlanguageapp/aktivity/InfoActivity.java
@@ -0,0 +1,17 @@
+package cz.ejstn.learnlanguageapp.aktivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cz.ejstn.learnlanguageapp.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+
+    }
+}
